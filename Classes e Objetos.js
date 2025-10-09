6ec9e7a70987f9ed9773a24a6ddc0e6a1f8516e0
tiagoalearruda <tiagoalearruda@gmail.com>
2025-10-09 20:48:05 -0300

    Class e Objeto

--- Classes e Objetos.js
+++ Classes e Objetos.js
@@ -5,11 +5,19 @@ class formaDeBolo {
         this.saborRecheio = saborRecheio
     }
     // função na class, não precisa ser declarada function
-    toString() {
+    toStringInfo() {
         console.log(`A massa desse bolo é de ${this.saborDaMassa} e o recheio é de ${this.saborRecheio}.`)
     }  
+    toStringAssar() {
+        console.log(`O bolo de ${this.saborDaMassa} e ${this.saborRecheio} está assando.`)
+    }
 }
 // sempre passar um let com = new "class"("")
 let boloPronto = new formaDeBolo("laranja","maçã")
+let boloPremium = new formaDeBolo("Baunilha","Coco")
 
-boloPronto.toString()
+boloPronto.toStringInfo()
+boloPronto.toStringAssar()
+console.log("--------")
+boloPremium.toStringInfo()
+boloPremium.toStringAssar()
