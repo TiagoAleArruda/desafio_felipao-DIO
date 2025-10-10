b019c387fc035e35a1c29b7bf630d28be10138ec
tiagoalearruda <tiagoalearruda@gmail.com>
2025-10-09 22:12:34 -0300

    Terceiro Desafio Felipão

--- terceiroDesafio.js
+++ terceiroDesafio.js
@@ -4,23 +4,12 @@ class heroi {
         this.idade = idade
         this.arma = arma
     }
-        // toString() {
-        //     if (this.tipo === "guerreiro") {
-        //         return(`O ${this.tipo} usou ${this.arma} `)
-        //     } else if (this.tipo === "mago") {
-        //        return(`O ${this.tipo} usou ${this.arma} `)
-        //     } else if (this.tipo === "monge") {
-        //         return(`O ${this.tipo} usou ${this.arma} `)
-        //     } else if (this.tipo === "ninja") {
-        //         return(`O ${this.tipo} usou ${this.arma} `)
-        //     } else {
-        //         return(`Tipo de herói desconhecido`)
-        //     }
-        // }
         atacar() {
+            // deixou o código mais escalonável
             const tiposHerois = ["guerreiro","mago","monge","ninja"]
+            
             if (tiposHerois.includes(this.tipo)) {
-                return(`O ${this.tipo} atacou usando ${this.arma}`)
+                return(`O ${this.tipo} atacou usando ${this.arma}.`)
             } else {
                 return 'Tipo de herói desconhecido'
             }
